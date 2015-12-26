496717320114c90d34b641116f937f9d37b2fa5d
dale tan <dale@1stdibs.com>
2015-12-26 00:42:22 -0500

    way to add caching from initial usage
    - mostly taken from here - https://github.com/divshot/cache-control
    - always force Cache-Control header

--- src/index.js
+++ src/index.js
@@ -8,18 +8,35 @@
 'use strict';
 
 const onHeaders = require('on-headers');
+const url = require('fast-url-parser');
+const globject = require('globject');
+const slasher = require('glob-slasher');
+const isEmpty = require('lodash.isempty');
 const cacheControl = require('./cacheControl');
 const { generateExpiresHeader, generateLastModifiedHeader } = require('./additionalHeaders');
 const utils = require('./utils');
 const timeValues = require('./timeValues');
 
+function isTrueObject(obj) {
+    return !Array.isArray(obj) && typeof obj === 'object';
+}
+
 function middleware(config) {
     return (req, res, next) => {
+
+        const { cacheSettings, paths } = config;
+        const pathname = url.parse(req.url).pathname;
+        const cacheValues = globject(slasher(paths || {}, {value: false}));
+        let cacheValue = cacheValues(slasher(pathname));
+
         onHeaders(res, () => {
-            if (config) {
-                const cache = cacheControl.generate(config);
-                res.setHeader(cache.headerName, cache.headerValue);
+            if (!isEmpty(cacheSettings) && isTrueObject(cacheSettings)) {
+                // override default cacheValue settings
+                cacheValue = cacheControl.generate(cacheSettings).headerValue;
+            } else if (!cacheValue) {
+                cacheValue = cacheControl.generate().headerValue;
             }
+            res.setHeader('Cache-Control', cacheValue);
         });
         next();
     };
